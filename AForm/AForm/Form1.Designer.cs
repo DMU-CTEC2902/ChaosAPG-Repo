748deae07ea8524f875b60a1ad7465ca27dee135
p17178463-Shridhar <p17178463@my365.dmu.ac.uk>
2019-02-04 15:41:51 +0000

    :)

--- AForm/AForm/Form1.Designer.cs
+++ AForm/AForm/Form1.Designer.cs
@@ -28,30 +28,30 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.lblCalmDown = new System.Windows.Forms.Label();
-            this.SuspendLayout();
-            // 
-            // lblCalmDown
-            // 
-            this.lblCalmDown.AutoSize = true;
-            this.lblCalmDown.Location = new System.Drawing.Point(74, 42);
-            this.lblCalmDown.Name = "lblCalmDown";
-            this.lblCalmDown.Size = new System.Drawing.Size(88, 13);
-            this.lblCalmDown.TabIndex = 0;
-            this.lblCalmDown.Text = "Calm Down Shrid";
-            // 
-            // Form1
-            // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
-            this.textBox1 = new System.Windows.Forms.TextBox();
-            this.SuspendLayout();
-            // 
-            // textBox1
-            // 
-            this.textBox1.Location = new System.Drawing.Point(130, 133);
-            this.textBox1.Name = "textBox1";
-            this.textBox1.Size = new System.Drawing.Size(100, 20);
-            this.textBox1.TabIndex = 0;
+            //this.lblCalmDown = new System.Windows.Forms.Label();
+            //this.SuspendLayout();
+            //// 
+            //// lblCalmDown
+            //// 
+            //this.lblCalmDown.AutoSize = true;
+            //this.lblCalmDown.Location = new System.Drawing.Point(74, 42);
+            //this.lblCalmDown.Name = "lblCalmDown";
+            //this.lblCalmDown.Size = new System.Drawing.Size(88, 13);
+            //this.lblCalmDown.TabIndex = 0;
+            //this.lblCalmDown.Text = "Calm Down Shrid";
+            //// 
+            //// Form1
+            //// 
+            //this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            //this.textBox1 = new System.Windows.Forms.TextBox();
+            //this.SuspendLayout();
+            //// 
+            //// textBox1
+            //// 
+            //this.textBox1.Location = new System.Drawing.Point(130, 133);
+            //this.textBox1.Name = "textBox1";
+            //this.textBox1.Size = new System.Drawing.Size(100, 20);
+            //this.textBox1.TabIndex = 0;
             // 
             // Form1
             // 
