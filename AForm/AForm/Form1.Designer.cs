08bc9fdfd15c3e50f1b5b5e3cb06233c1c8095c9
P17181674 <P17181674@GWHXX653WIN008W>
2019-02-04 15:38:18 +0000

    Label added

--- AForm/AForm/Form1.Designer.cs
+++ AForm/AForm/Form1.Designer.cs
@@ -28,13 +28,34 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.lblCalmDown = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // lblCalmDown
+            // 
+            this.lblCalmDown.AutoSize = true;
+            this.lblCalmDown.Location = new System.Drawing.Point(74, 42);
+            this.lblCalmDown.Name = "lblCalmDown";
+            this.lblCalmDown.Size = new System.Drawing.Size(88, 13);
+            this.lblCalmDown.TabIndex = 0;
+            this.lblCalmDown.Text = "Calm Down Shrid";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.lblCalmDown);
+            this.Name = "Form1";
             this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Label lblCalmDown;
     }
 }
 
