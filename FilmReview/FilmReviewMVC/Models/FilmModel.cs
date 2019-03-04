e065860cdb0e6b3f8a4efcdeb99d753cb8b0e3f0
p17178463-Shridhar <p17178463@my365.dmu.ac.uk>
2019-03-04 15:58:34 +0000

    Added FilmModel class

---
+++ FilmReview/FilmReviewMVC/Models/FilmModel.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace FilmReviewMVC.Models
+{
+    public class FilmModel
+    {
+        public virtual int FilmId { get; set; }
+        public virtual int ActorId { get; set; }
+        public virtual int DirtectorId { get; set; }
+        public virtual int GenreId { get; set; }
+        public virtual string FilmName { get; set; }
+        public virtual DateTime ReleaseDate { get; set; }
+        public virtual string Rating { get; set; }
+        public virtual string Runtime { get; set; }
+        public virtual string Studio { get; set; }
+
+    }
+}
