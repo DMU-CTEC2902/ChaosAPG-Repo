9bed03ff0ebcdc88b164a030be4d81925d5edb9a
p17218124 <p17218124@my365.dmu.ac.uk>
2019-03-04 16:09:13 +0000

    Added actorModel class

---
+++ FilmReview/FilmReviewMVC/Models/ActorModel.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace FilmReviewMVC.Models
+{
+    public class ActorModel
+    {
+        public virtual int ActorId { get; set; }
+        public virtual string ActorName{ get; set; }
+        public virtual int DOB { get; set; }
+        public virtual int Nationality { get; set; }
+        public virtual string HighestRatedMovie { get; set; }
+        public virtual string Description { get; set; }
+      
+    }
+}
