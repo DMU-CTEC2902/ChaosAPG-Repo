5f24e8bdea2b6d657f654a446a816af6e2acfac3
p17178463-Shridhar <p17178463@my365.dmu.ac.uk>
2019-03-04 15:15:22 +0000

    Created MVC project

--- AForm/AForm/Form1.Designer.cs
+++ AForm/AForm/Form1.Designer.cs
@@ -28,36 +28,36 @@
         /// </summary>
         private void InitializeComponent()
         {
-            //this.lblCalmDown = new System.Windows.Forms.Label();
-            //this.SuspendLayout();
-            //// 
-            //// lblCalmDown
-            //// 
-            //this.lblCalmDown.AutoSize = true;
-            //this.lblCalmDown.Location = new System.Drawing.Point(74, 42);
-            //this.lblCalmDown.Name = "lblCalmDown";
-            //this.lblCalmDown.Size = new System.Drawing.Size(88, 13);
-            //this.lblCalmDown.TabIndex = 0;
-            //this.lblCalmDown.Text = "Calm Down Shrid";
-            //// 
-            //// Form1
-            //// 
-            //this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
-            //this.textBox1 = new System.Windows.Forms.TextBox();
-            //this.SuspendLayout();
-            //// 
-            //// textBox1
-            //// 
-            //this.textBox1.Location = new System.Drawing.Point(130, 133);
-            //this.textBox1.Name = "textBox1";
-            //this.textBox1.Size = new System.Drawing.Size(100, 20);
-            //this.textBox1.TabIndex = 0;
+            this.button1 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(109, 54);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(184, 23);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Shrid\'s Button";
+            this.button1.UseVisualStyleBackColor = true;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(349, 201);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(76, 13);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "MyNew button";
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
             this.Text = "Form1";
             this.ResumeLayout(false);
             this.PerformLayout();
@@ -65,6 +65,9 @@
         }
 
         #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label1;
     }
 }
 
