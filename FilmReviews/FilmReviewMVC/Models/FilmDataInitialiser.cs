8e7f1d3919e5df5303f057b729a6c2d5cc63b6cf
p17218124 <p17218124@my365.dmu.ac.uk>
2019-03-18 17:07:39 +0000

    improved the film Initialiser

--- FilmReviews/FilmReviewMVC/Models/FilmDataInitialiser.cs
+++ FilmReviews/FilmReviewMVC/Models/FilmDataInitialiser.cs
@@ -11,26 +11,27 @@ namespace FilmReviewMVC.Models
         protected override void Seed(FilmContext context)
         {
             
-
+            //this is an object of genre
             Genre genre1 = new Genre();
+            //object of genreID
             genre1.GenreID = 1;
             genre1.GenreType = "Comedy";
             context.Genres.Add(genre1);
 
-            Genre genre1 = new Genre();
-            genre1.GenreID = 1;
-            genre1.GenreType = "Horror";
-            context.Genres.Add(genre1);
+            Genre genre2 = new Genre();
+            genre2.GenreID = 1;
+            genre2.GenreType = "Horror";
+            context.Genres.Add(genre2);
 
-            Genre genre1 = new Genre();
-            genre1.GenreID = 1;
-            genre1.GenreType = "Documentary";
-            context.Genres.Add(genre1);
+            Genre genre3 = new Genre();
+            genre3.GenreID = 1;
+            genre3.GenreType = "Documentary";
+            context.Genres.Add(genre3);
 
-            Genre genre1 = new Genre();
-            genre1.GenreID = 1;
-            genre1.GenreType = "Sci-fi";
-            context.Genres.Add(genre1);
+            Genre genre4 = new Genre();
+            genre4.GenreID = 1;
+             genre4.GenreType = "Sci-fi";
+            context.Genres.Add(genre4);
 
 
             Actor actor1 = new Actor();
@@ -39,23 +40,23 @@ namespace FilmReviewMVC.Models
             actor1.DOB = new DateTime(2012, 12, 12);
             context.Actors.Add(actor1);
 
-            Actor actor1 = new Actor();
-            actor1.ActorID = 1;
-            actor1.ActorName = "Mark";
-            actor1.DOB = new DateTime(2013, 12, 12);
-            context.Actors.Add(actor1);
+            Actor actor2 = new Actor();
+            actor2.ActorID = 1;
+            actor2.ActorName = "Mark";
+            actor2.DOB = new DateTime(2013, 12, 12);
+            context.Actors.Add(actor2);
 
-            Actor actor1 = new Actor();
-            actor1.ActorID = 1;
-            actor1.ActorName = "Josh";
-            actor1.DOB = new DateTime(2014, 12, 12);
-            context.Actors.Add(actor1);
+            Actor actor3 = new Actor();
+            actor3.ActorID = 1;
+            actor3.ActorName = "Josh";
+            actor3.DOB = new DateTime(2014, 12, 12);
+            context.Actors.Add(actor3);
 
-            Actor actor1 = new Actor();
-            actor1.ActorID = 1;
-            actor1.ActorName = "Paul";
-            actor1.DOB = new DateTime(2012, 12, 12);
-            context.Actors.Add(actor1);
+            Actor actor4 = new Actor();
+            actor4.ActorID = 1;
+            actor4.ActorName = "Paul";
+            actor4.DOB = new DateTime(2012, 12, 12);
+            context.Actors.Add(actor4);
 
             //first director 
 
@@ -69,14 +70,16 @@ namespace FilmReviewMVC.Models
             context.Directors.Add(director1);
 
             //second director
-            Director director1 = new Director();
+            Director director2 = new Director();
             //showing the director ID
-            director1.DirectorID = 1;
+            director2.DirectorID = 1;
             //showing the directors name
-            director1.DirectorName = "Alex";
+            director2.DirectorName = "Alex";
             //using the DateTime variable for the directors film date
-            director1.DOB = new DateTime(2019, 12, 12);
-            context.Directors.Add(director1);
+            director2.DOB = new DateTime(2019, 12, 12);
+            context.Directors.Add(director2);
+
+            //first film 
 
             Film film1 = new Film();
             //Variable to store the Film ID and display it
@@ -100,87 +103,87 @@ namespace FilmReviewMVC.Models
             ////Code responsible for adding the first film 
             context.Films.Add(film1);
 
-            Film film1 = new Film();
+            Film film2 = new Film();
             //Variable to store the Film ID and display it
-            film1.FilmID = 1;
+            film2.FilmID = 1;
             //Variable to store the Genre ID and display it
-            film1.GenreID = 1;
+            film2.GenreID = 1;
             //Variable to store the Actor ID and display it
-            film1.ActorID = 1;
+            film2.ActorID = 1;
             //Variable to store the Director ID and display it
-            film1.DirectorID = 1;
+            film2.DirectorID = 1;
             //Variable to store Film Name
-            film1.FilmName = "Avengers Assemble";
+            film2.FilmName = "Avengers Assemble";
             //Variable to store Release Date
-            film1.ReleaseDate = new DateTime(2014, 12, 12);
+            film2.ReleaseDate = new DateTime(2014, 12, 12);
             //Variable to store the Rating
-            film1.Rating = "5";
+            film2.Rating = "5";
             //Variable to store the RunTime
-            film1.Runtime = "2 hour";
+            film2.Runtime = "2 hour";
             //Variable to store the Studio 
-            film1.Studio = "Warner Brothers";
+            film2.Studio = "Warner Brothers";
             ////Code responsible for adding the first film 
-            context.Films.Add(film1);
+            context.Films.Add(film2);
 
-            Film film1 = new Film();
+            Film film3 = new Film();
             //Variable to store the Film ID and display it
-            film1.FilmID = 1;
+            film3.FilmID = 1;
             //Variable to store the Genre ID and display it
-            film1.GenreID = 1;
+            film3.GenreID = 1;
             //Variable to store the Actor ID and display it
-            film1.ActorID = 1;
+            film3.ActorID = 1;
             //Variable to store the Director ID and display it
-            film1.DirectorID = 1;
+            film3.DirectorID = 1;
             //Variable to store Film Name
-            film1.FilmName = "Spiderman";
+            film3.FilmName = "Spiderman";
             //Variable to store Release Date
-            film1.ReleaseDate = new DateTime(2015, 12, 12);
+            film3.ReleaseDate = new DateTime(2015, 12, 12);
             //Variable to store the Rating
-            film1.Rating = "5";
+            film3.Rating = "5";
             //Variable to store the RunTime
-            film1.Runtime = "3 hour";
+            film3.Runtime = "3 hour";
             //Variable to store the Studio 
-            film1.Studio = "Orange fox studios";
+            film3.Studio = "Orange fox studios";
             ////Code responsible for adding the first film 
-            context.Films.Add(film1);
+            context.Films.Add(film3);
 
             ////Second Film
 
 
 
 
-            Film film2 = new Film();
+            Film film4 = new Film();
             //Variable to store the Film ID and display it
-            film2.FilmID = 2;
+            film4.FilmID = 2;
             //Variable to store Film Name
-            film2.FilmName = "Captain Marvel";
+            film4.FilmName = "Captain Marvel";
             //Variable to store Release Date
-            film2.ReleaseDate = Convert.ToDateTime("08/03/2019");
+            film4.ReleaseDate = Convert.ToDateTime("08/03/2019");
             //Variable to store the Rating
-            film2.Rating = "7";
+            film4.Rating = "7";
             //Variable to store the RunTime
-            film2.Runtime = "2 hours";
+            film4.Runtime = "2 hours";
             //Variable to store the Studio 
-            film2.Studio = "Marvel";
+            film4.Studio = "Marvel";
             //Code responsible for adding the first film 
-            context.Films.Add(film1);
+            context.Films.Add(film4);
 
             //Third Film
-            Film film3 = new Film();
+            Film film5 = new Film();
             //Variable to store the Film ID and display it
-            film1.FilmID = 3;
+            film5.FilmID = 3;
             //Variable to store Film Name
-            film1.FilmName = "Black Panther";
+            film5.FilmName = "Black Panther";
             //Variable to store Release Date
-            film1.ReleaseDate = Convert.ToDateTime("12/10/2012");
+            film5.ReleaseDate = Convert.ToDateTime("12/10/2012");
             //Variable to store the Rating
-            film1.Rating = "7";
+            film5.Rating = "7";
             //Variable to store the RunTime
-            film1.Runtime = "2 hours";
+            film5.Runtime = "2 hours";
             //Variable to store the Studio 
-            film1.Studio = "Marvel";
+            film5.Studio = "Marvel";
             //Code responsible for adding the first film 
-            context.Films.Add(film1);
+            context.Films.Add(film5);
             base.Seed(context);
 
         }
